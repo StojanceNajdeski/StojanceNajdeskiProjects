1ef8a4257f66db95f111b00addae6cb776463703
unknown <stojancenajdeski55@gmail.com>
2025-06-27 20:46:51 +0200

    started database

--- menu-application-v1.0/src/app/bakardiAdminPanel/page.tsx
+++ menu-application-v1.0/src/app/bakardiAdminPanel/page.tsx
@@ -1,109 +1,100 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 import "../../../i18n";
 import { useTranslation } from "react-i18next";
-import Image from "next/image";
+import ProtectedRoute from "../ProtectedRoute";
 
 export default function BakardiAdminPanel() {
   const router = useRouter();
-  const { t, i18n } = useTranslation();
-  useEffect(() => {
-    const loggedIn = localStorage.getItem("bakardiAdminLoggedIn");
-    if (loggedIn !== "true") {
-      router.replace("/bakardiAdminLogin");
-    }
-  }, [router]);
+  const handleLogout = () => {
+    localStorage.removeItem("bakardiAdminLoggedIn");
+    router.push("/bakardiAdminLogin");
+  };
+  const { t } = useTranslation();
 
   return (
-    <div className="p-10">
-      <div className="flex">
-        <h1 className="text-3xl font-bold mb-6">
-          Добредојдовте во Админ Панелот
-        </h1>
-      </div>
-      <button className="bg-red-600">Одјави се</button>
-      <div className="text-base sm:text-lg md:text-xl lg:text-2xl justify-center items-center flex flex-col">
-        <div className="flex w-10 h-15 gap-2 justify-center items-center">
-          <Image
-            src="/english.png"
-            alt="englishLanguageLogo"
-            className="mt-4"
-            width={40}
-            height={50}
-            onClick={() => i18n.changeLanguage("en")}
-          />
-          <Image
-            src="/flag-north-macedonia_1f1f2-1f1f0.png"
-            alt="macedoniaLanguageLogo"
-            className="mt-4"
-            width={40}
-            height={50}
-            onClick={() => i18n.changeLanguage("mk")}
-          />
-          <Image
-            src="/albania.png"
-            alt="albaniaLanguageLogo"
-            className="mt-4"
-            width={40}
-            height={50}
-            onClick={() => i18n.changeLanguage("al")}
-          />
+    <ProtectedRoute>
+      <div className="p-5">
+        <div className="flex justify-center items-center">
+          <button
+            className="bg-red-600 text-white py-2 px-5 rounded-md"
+            onClick={handleLogout}
+          >
+            Одјави се
+          </button>
         </div>
-        <h1 className="text-center py-4 text-3xl">{t("bakardiMeni")}</h1>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/specijaliteti">{t("specijaliteti")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/novo">{t("novo")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/pojadok">{t("pojadok")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/dodatoci">{t("dodatoci")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/panceroti">{t("panceroti")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="furnarinki">{t("furnarinki")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="meze">{t("meze")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="sirenje">{t("sirenje")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/salati">{t("salati")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/pasta">{t("pasta")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/pici">{t("pici")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/rolsendvici">{t("rolSendvici")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/burgeri">{t("burgeri")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/sendvici">{t("sendvici")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/daski">{t("daski")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/pastrmajlija">{t("pastrmajlija")}</Link>
-        </button>
-        <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
-          <Link href="/deserti">{t("deserti")}</Link>
-        </button>
+        <div className="flex justify-center items-center mt-4">
+          <h1 className="text-3xl font-bold mb-2 text-center">
+            Добредојдовте во <br /> Админ Панелот
+          </h1>
+        </div>
+        <div className="text-base sm:text-lg md:text-xl lg:text-2xl justify-center items-center flex flex-col">
+          <h1 className="text-center py-4 text-3xl">{t("bakardiMeni")}</h1>
+          <div className="flex justify-center items-center">
+            <button className="bg-orange-600 text-white p-3 w-50 mb-5 rounded-2xl">
+              Додади Категорија
+            </button>
+          </div>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/specijaliteti">
+              {t("specijaliteti")}
+            </Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/novo">{t("novo")}</Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/pojadok">{t("pojadok")}</Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/dodatoci">{t("dodatoci")}</Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/panceroti">{t("panceroti")}</Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/furnarinki">{t("furnarinki")}</Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/meze">{t("meze")}</Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/sirenje">{t("sirenje")}</Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/salati">{t("salati")}</Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/pasta">{t("pasta")}</Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/pici">{t("pici")}</Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/rolsendvici">
+              {t("rolSendvici")}
+            </Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/burgeri">{t("burgeri")}</Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/sendvici">{t("sendvici")}</Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/daski">{t("daski")}</Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/pastrmajlija">
+              {t("pastrmajlija")}
+            </Link>
+          </button>
+          <button className="bg-red-400 text-white p-3 w-50 mb-5 rounded-2xl">
+            <Link href="/bakardiAdminPanel/deserti">{t("deserti")}</Link>
+          </button>
+        </div>
       </div>
-    </div>
+    </ProtectedRoute>
   );
 }
